9103dfeadac0f98f156dd89f8cfae5d1bf0da7ab
Tai Nguyen <taikeo591@gmail.com>
2024-06-24 23:50:57 +0700

    update

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,7 +7,5 @@ F
 _387_First_Unique.java,c\f\cff497259e3e73d7d2694c4583f2f1f7ec820dca
 F
 _704_SearchBinary.java,a\d\ad98e0ae6ca63d8c81387b55bf0a7db35c92eed0
-M
-_704_BinarySearchByDeQuy.java,2\7\27457b64e11b2ac7585986115d90271dfca9eb6b
 C
 _912_MergeSort.java,4\b\4bfbb43006cb261e5aba9ee3805ee0c9fd0edc03
--- _912_MergeSort.java
+++ _912_MergeSort.java
@@ -1,4 +1,79 @@
 public class _912_MergeSort {
 
+    public int [] merge(int [] a1, int [] a2) {
+        int n =a1.length + a2.length;
+        int [] result = new int[n];
+
+        int i = 0, i1 = 0,i2= 0;
+        while(i < n) {
+            if (i1 < a1.length && i2 < a2.length) {
+                if (a1[i1] < a2[i2]) {
+                    result[i] = a1[i1];
+                    i++;
+                    i1++;
+                } else {
+                    result[i] = a2[i2];
+                    i++;
+                    i2++;
+                }
+            } else if (i1 < a1.length) {
+                result[i] = a1[i1];
+                i++;
+                i1++;
+            } else {
+                result[i] = a2[i2];
+                i++;
+                i2++;
+            }
+        }
+        return result;
+    }
+
+
+    public int[] mergeSort(int a[], int L, int R) {
+        // THDB DKD
+
+        if (L > R) {
+            return new int[0];
+        }
+
+        if (L == R) {
+            int [] singleElement = {a[L]};
+            return singleElement;
+
+        }
+
+        // thqt
+
+        //chia ra
+        int k = (L + R) / 2;
+
+        int [] a1 = mergeSort(a, L, k);
+
+        int [] a2 = mergeSort(a, k + 1, R);
+
+        // tron vào
+
+
+
+        int [] result = merge(a1, a2);
+        return result;
+
+    }
+
+    public int[]  sortArray(int[] nums){
+        return mergeSort(nums, 0, nums.length-1 );
+    }
+
+    public static void main(String[] args) {
+        _912_MergeSort obj = new _912_MergeSort();
+        int [] a = {5, 2, 3, 1};
+        int [] result = obj.sortArray(a);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+
+
 
 }
